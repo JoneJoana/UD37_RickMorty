a38523bbc3b4caebdc33f0707eb0164766c8ef6e
Joana <joanajanefdez@gmail.com>
2022-09-15 14:55:29 +0200

    add-character vista i logica hecha

--- src/app/add-character/add-character.component.ts
+++ src/app/add-character/add-character.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Character } from '../models/characters.model';
+import { RickMortyService } from '../rick-morty.service';
 
 @Component({
   selector: 'app-add-character',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCharacterComponent implements OnInit {
 
-  constructor() { }
+  newCharacter: Character = {}
+
+  constructor(private _router: Router, private rickMortyService: RickMortyService) { }
 
   ngOnInit(): void {
   }
 
+  onBack(): void{
+    this._router.navigate(['/characters']);
+  }
+
+  save(): void{
+    const data = {
+      id: this.newCharacter.id,
+      name: this.newCharacter.name,
+      status: this.newCharacter.status,
+      species: this.newCharacter.species,
+      gender: this.newCharacter.gender,
+      origin: this.newCharacter.origin,
+      location: this.newCharacter.location
+    }
+    this.rickMortyService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          alert('Personaje guardado')
+          this._router.navigate(['/characters']);
+        },error =>{
+          console.log(error+' save() NewCharacter')
+        }
+      );
+
+  }
 }
